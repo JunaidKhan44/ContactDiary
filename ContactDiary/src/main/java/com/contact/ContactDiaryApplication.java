7357edcd08ac70747ced6831859ff757e8151f86
JunaidKhan44 <junadmanimani555@gmail.com>
2022-10-01 10:59:26 +0500

    First Commit

---
+++ ContactDiary/src/main/java/com/contact/ContactDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactDiaryApplication.class, args);
+	}
+
+}
